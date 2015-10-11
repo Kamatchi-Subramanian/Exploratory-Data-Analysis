8ab971b676b4c7eed5cc2a472ed37646b0f4beaa
Kamatchi-Subramanian <pksubramanian12@yahoo.com>
2015-10-11 10:20:47 -0700

    Made change to plot1.R
    Changed comment in Line 1 to say Global Active power instead of Global
    Reactive power

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-#Plot histogram of Global Reactive Power. 
+#Plot histogram of Global Active Power. 
 #Copy in to the png format and assign a file name. 
 hist(c5$Global_active_power, main="Global Active Power", xlab="Glboal Active Power(kilowatts)", col = "red")
 dev.copy(png, file="plot1.png")
